8e9826af9e6769cec6228cb93789b75e9f2f8e17
AustinHongLee <lizonghong084@gmail.com>
2023-09-28 07:49:09 +0800

    Vscode in

--- Hello.py
+++ Hello.py
@@ -1,46 +1,28 @@
-import openpyxl 
-import os 
-import tkinter as tk 
-from tkinter import messagebox, StringVar 
+import openpyxl
+import os
+import tkinter as tk
+from tkinter import messagebox, StringVar, Text
 
-current_row = 1  # 全局變數，用於追踪目前正在寫入的行
+# 定義添加選擇到文本框的函數
+def add_to_textbox():
+    current_value = selected_option.get()
+    textbox.insert(tk.END, current_value + "\n")
 
-def create_excel(): 
-    global current_row  # 使用全局變數
-    file_name = "new_blank_workbook.xlsx"
-
-    if os.path.exists(file_name):
-        wb = openpyxl.load_workbook(file_name)  # 如果文件存在，加載它
-        ws = wb.active
-    else:
-        print("創建一個新的工作簿...") 
-        wb = openpyxl.Workbook() 
-        ws = wb.active 
-
-    print("向工作簿中添加數據...") 
-    ws[f"A{current_row}"].value = selected_option.get() 
-    ws[f"B{current_row}"].value = f"=LEFT(A{current_row},1)" 
-    current_row += 1  # 在選擇後增加行數
-
-    print("保存工作簿到一個文件...") 
-    wb.save(file_name) 
-    print(f"保存工作簿到 {os.getcwd()}\\{file_name}...") 
-
-    print("打開新創建的工作簿...") 
-    os.startfile(file_name) 
-
+# 定義保存到Excel的函數
+def save_to_excel():
+    wb = openpyxl.Workbook()
+    ws = wb.active
+    lines = textbox.get(1.0, tk.END).split("\n")
+    for i, line in enumerate(lines, 1):
+        if line:  # 僅當line非空時寫入Excel
+            ws[f"A{i}"].value = line
+    wb.save("saved_values.xlsx")
 
 window = tk.Tk()
 window.title("軟體視窗")
-window.geometry("280x400+{}+{}".format(window.winfo_screenwidth() // 2 - 200, window.winfo_screenheight() // 2 - 200))
+window.geometry("280x400")
 
-
-# 在視窗中加入一個標籤，提示用戶輸入關鍵字
-label = tk.Label(window, text="選擇Support類型", font=("標楷體", 20))
-label.place(relx=0.5, rely=0.2, anchor='center')  # 位置設定為視窗的中央
-label.pack()  # 設定佈局
-
-
+# 下拉選項
 options_part1 = ["C015", "C016", "C017", "C019", "C021"]
 options_part2 = ["C022", "C023", "C040", "C041", "C046"]
 options_part3 = ["C1021", "C1022", "C1121", "C1122", "C1301"]
@@ -50,15 +32,24 @@ options_part6 = ["W117", "W122", "W124", "W127", "W131"]
 options_part7 = ["W132", "W140", "W141"]
 
 options = options_part1 + options_part2 + options_part3 + options_part4 + options_part5 + options_part6 + options_part7
+ 
 
 
 selected_option = StringVar(window)
 selected_option.set(options[0])
-
 dropdown = tk.OptionMenu(window, selected_option, *options)
-dropdown.place(relx=0.5, rely=0.6, anchor='center')
+dropdown.pack(pady=10)
 
-button = tk.Button(window, text="創建Excel", command=create_excel)
-button.place(relx=0.5, rely=0.8, anchor='center')
+# 按鈕用來添加選擇到文本框
+add_button = tk.Button(window, text="添加到文本框", command=add_to_textbox)
+add_button.pack(pady=10)
+
+# 文本框用於顯示添加的值
+textbox = Text(window, height=10, width=30)
+textbox.pack(pady=10)
+
+# 按鈕用來保存文本框的值到Excel
+save_button = tk.Button(window, text="保存到Excel", command=save_to_excel)
+save_button.pack(pady=10)
 
 window.mainloop()
