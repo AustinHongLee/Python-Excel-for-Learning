bfa4e55c34898e2b7915d0eef7be636110ca0cb9
HomePlace <lizonghong084@gmail.com>
2023-10-03 00:27:45 +0800

    將Keywords活用化Ver.01 歸類主標題關鍵字 增加新example.xlsx文件判斷法則

--- Pratice_difflib_Fuction_V2.py
+++ Pratice_difflib_Fuction_V2.py
@@ -3,11 +3,68 @@ import difflib
 import sys
 import re
 import codecs
+import os
 
 sys.stdout = codecs.getwriter("utf-8")(open('output.txt', 'w'))
 sys.stdout = open('output.txt', 'w')
 import re
 
+
+"""⬇️ 這邊以下是主體的關鍵字列表 ⬇️"""
+def Get_Main_material_title_Keywords():
+    """返回所有关键字的列表"""
+    # 分类关键字
+    pipes_and_fittings = [
+        "Pipe", "Elbow", "Reducer", "Tee", "Nipple", "Union", "Coupling", "Plug", 
+        "Cap", "Stub End", "Swage Nipple", "Concentric Swage", "Eccentric Swage", 
+        "Cross", "Olet", "Bend", "Miter Bend", "Lateral"
+    ]
+
+    flanges = [
+        "Weld Neck Flange", "Socket Weld Flange", "Slip-On Flange", "Lap Joint Flange", 
+        "Threaded Flange", "Blind Flange", "Orifice Flange", "Spectacle Blind", 
+        "Spacer", "Ring Joint Flange", "LWN Flange", "Square Flange"
+    ]
+
+    valves = [
+        "Butterfly Valve", "Ball Valve", "Gate Valve", "Dual Plate Check Valve", 
+        "Globe Valve", "Needle Valve", "Check Valve", "Diaphragm Valve", "Plug Valve", 
+        "Relief Valve", "Safety Valve", "Solenoid Valve", "Pressure Reducing Valve"
+    ]
+
+    control_valves = [
+        "Globe Control Valve", "Butterfly Control Valve", "Ball Control Valve", 
+        "Diaphragm Control Valve", "Pinch Control Valve", "Gate Control Valve", 
+        "Rotary Control Valve", "Self-operated Control Valve", "Pilot-operated Control Valve",
+        "Sliding Cylinder Control Valve", "Flapper-nozzle Control Valve", 
+        "Jet Pipe Control Valve", "Eccentric Disk Control Valve", "Angle Control Valve"
+    ]
+
+    bolts_and_gaskets = [
+        "Bolt", "Nut", "Gasket", "Ring Gasket", "Spiral Wound Gasket", 
+        "Metallic Gasket", "Non-Metallic Gasket"
+    ]
+
+    misc = [
+        "Support", "Sockolet", "Weldolet", "Expansion Joint", "Strainer", 
+        "Blind", "Spacer", "Rupture Disc"
+    ]
+
+    # 组合所有关键字
+    keywords = pipes_and_fittings + flanges + valves + control_valves + bolts_and_gaskets + misc
+
+    return keywords    
+"""⬆️ 這邊以上是主體的關鍵字列表 ⬆️"""
+
+
+
+
+
+
+
+
+keywords = Get_Main_material_title_Keywords()
+#keywords = pipes_and_fittings + flanges + valves + control_valves + bolts_and_gaskets + misc
 def exact_match_start(cell_value, keywords):
     """進行開頭匹配"""
     if cell_value is None:
@@ -55,53 +112,19 @@ file_path = (
     r'C:\Users\a0976\OneDrive\文件\GitHub\Python Excel for Learning'
     r'\example.xlsx'
 )
-wb = openpyxl.load_workbook(file_path)
+try:
+    wb = openpyxl.load_workbook(file_path)
+except FileNotFoundError:
+    print(f"找不到文件 {file_path}，正在嘗試使用本地資料夾內的 example.xlsx 檔案...")
+    file_path = os.path.join(os.getcwd(), 'example.xlsx')
+    wb = openpyxl.load_workbook(file_path)
 ws = wb.active
 print("Excel文件成功加載。")
 
-# 模糊匹配的關鍵字列表
-# 分類關鍵字
-pipes_and_fittings = [
-    "Pipe", "Elbow", "Reducer", "Tee", "Nipple", "Union", "Coupling", "Plug", 
-    "Cap", "Stub End", "Swage Nipple", "Concentric Swage", "Eccentric Swage", 
-    "Cross", "Olet", "Bend", "Miter Bend", "Lateral"
-]
-
-flanges = [
-    "Weld Neck Flange", "Socket Weld Flange", "Slip-On Flange", "Lap Joint Flange", 
-    "Threaded Flange", "Blind Flange", "Orifice Flange", "Spectacle Blind", 
-    "Spacer", "Ring Joint Flange", "LWN Flange", "Square Flange"
-]
-
-valves = [
-    "Butterfly Valve", "Ball Valve", "Gate Valve", "Dual Plate Check Valve", 
-    "Globe Valve", "Needle Valve", "Check Valve", "Diaphragm Valve", "Plug Valve", 
-    "Relief Valve", "Safety Valve", "Solenoid Valve", "Pressure Reducing Valve"
-]
-
-control_valves = [
-    "Globe Control Valve", "Butterfly Control Valve", "Ball Control Valve", 
-    "Diaphragm Control Valve", "Pinch Control Valve", "Gate Control Valve", 
-    "Rotary Control Valve", "Self-operated Control Valve", "Pilot-operated Control Valve",
-    "Sliding Cylinder Control Valve", "Flapper-nozzle Control Valve", 
-    "Jet Pipe Control Valve", "Eccentric Disk Control Valve", "Angle Control Valve"
-]
-
-bolts_and_gaskets = [
-    "Bolt", "Nut", "Gasket", "Ring Gasket", "Spiral Wound Gasket", 
-    "Metallic Gasket", "Non-Metallic Gasket"
-]
-
-misc = [
-    "Support", "Sockolet", "Weldolet", "Expansion Joint", "Strainer", 
-    "Blind", "Spacer", "Rupture Disc"
-]
-
-# 組合所有關鍵字
-keywords = pipes_and_fittings + flanges + valves + control_valves + bolts_and_gaskets + misc
 
 
-print("開始迭代工作表中的{ws.max_row}行...")
+
+print(f"開始迭代工作表中的{ws.max_row}行...")
 for row in ws.iter_rows(min_row=1, max_row=ws.max_row, min_col=1, max_col=1):
     cell = row[0]
     try:
@@ -128,3 +151,5 @@ print("Excel文件保存成功。")
 # 讀取輸出文件並輸出到控制台
 with open('output.txt', 'r') as f:
     print(f.read())
+    
+
